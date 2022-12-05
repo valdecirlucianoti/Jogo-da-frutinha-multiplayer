85c10cb6439eeb25327a7f48dd6f926c5421c8ad
Valdecir Luciano <valdecirlucianoti@gmail.com>
2022-12-05 15:26:47 -0300

    Adicionado o Socket.io e Nodemon

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-import express from "express";
+//import { create } from "domain";
+import express from 'express';
 import http from 'http';
+import createGame from "./public/game.js";
+import { Server } from "socket.io"
 
 const app = express();
 const server = http.createServer(app);
+const sockets = new Server(server);
 
 app.use(express.static('public'));
+
+const game = createGame();
+
+console.log(game.state);
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id;
+    console.log(`> Player connected on Server with id: ${playerId}`);
+
+    socket.emit('setup', game.state)
+});
 
 server.listen(3000, () => {
     console.log(`> Server listening on port: 3000`);
